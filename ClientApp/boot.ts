50343c47d088e6bb0e1512c4844750bc5c812ffa
tmacharia <timothy.macharia@outlook.com>
2019-09-02 02:17:19 +0300

    removes unncessary boilerplate code & updates project properties

--- ClientApp/boot.ts
+++ ClientApp/boot.ts
@@ -6,8 +6,6 @@ Vue.use(VueRouter);
 
 const routes = [
     { path: '/', component: require('./components/home/home.vue.html') },
-    { path: '/counter', component: require('./components/counter/counter.vue.html') },
-    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') }
 ];
 
 new Vue({
