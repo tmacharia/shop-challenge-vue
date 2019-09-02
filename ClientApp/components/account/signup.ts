48789463f7400ba1469cce96b4dad299660719c7
tmacharia <timothy.macharia@outlook.com>
2019-09-02 14:36:17 +0300

    finalizes feature: user sign-up

--- ClientApp/components/account/signup.ts
+++ ClientApp/components/account/signup.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
+import Axios from 'axios';
 
 interface User {
     Email: string;
@@ -14,5 +15,24 @@ export default class SignUpComponent extends Vue {
 
     mounted() {
         document.title = this.title;
+    };
+    submitForm(e: Event) {
+        e.preventDefault();
+        this.error = '';
+        Axios.post('/account/register', this.user)
+            .then(response => {
+                alert('Account created successfully. Redirecting you to login page...');
+                window.location.href = '/signin';
+            })
+            .catch(error => {
+                if (error.response.status === 400) {
+                    var errors = error.response.data;
+                    if (errors.length) {
+                        this.error = errors[0];
+                    }
+                } else {
+                    this.error = 'An error occured';
+                }
+            })
     }
 }
