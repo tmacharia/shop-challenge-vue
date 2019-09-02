67115d5e3987caa88a714b283f9549683087b8f1
tmacharia <timothy.macharia@outlook.com>
2019-09-02 14:33:45 +0300

    sets password contraint to min 6 characters

--- ViewModels/UserViewModel.cs
+++ ViewModels/UserViewModel.cs
@@ -9,6 +9,7 @@ namespace Shop.Web.ViewModels
         public string Email { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Password must have atleast 6 characters")]
         public string Password { get; set; }
     }
 }
