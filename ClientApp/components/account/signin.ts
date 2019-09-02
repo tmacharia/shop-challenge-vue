1e816798b5c89fc8c178f8384678a0a2f2cbb9e0
tmacharia <timothy.macharia@outlook.com>
2019-09-02 14:35:14 +0300

    finalizes feature: user sign-in

--- ClientApp/components/account/signin.ts
+++ ClientApp/components/account/signin.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
+import Axios from 'axios';
+import $ from 'jquery';
 
 interface User {
     Email: string;
@@ -15,5 +17,23 @@ export default class SignInComponent extends Vue {
 
     mounted() {
         document.title = this.title;
+    };
+    submitForm(e: Event) {
+        e.preventDefault();
+        this.error = '';
+        Axios.post('/account/login', this.user)
+            .then(response => {
+                window.location.href = '/';
+            })
+            .catch(error => {
+                if (error.response.status === 400) {
+                    var errors = error.response.data;
+                    if (errors.length) {
+                        this.error = errors[0];
+                    }
+                } else {
+                    this.error = 'An error occured';
+                }
+            })
     }
 }
