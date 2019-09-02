d174c1a9c9d955b95ee60f4fc0889efc34d9648b
tmacharia <timothy.macharia@outlook.com>
2019-09-02 09:30:08 +0300

    minor edits

--- Globals.cs
+++ Globals.cs
@@ -1,7 +1,12 @@
-﻿namespace Shop.Web
+﻿using System;
+
+namespace Shop.Web
 {
     public static class Globals
     {
         public const string AppName = "Shop Viewer";
+
+        public static Random random = new Random();
+        public static int RandomNum(int max = 1000) => random.Next(0, max);
     }
 }
