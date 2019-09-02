67d2e086ba3c41082f9167933c01d2b995aba89b
tmacharia <timothy.macharia@outlook.com>
2019-09-02 16:13:29 +0300

    fixes vue routing issue with setting active nav-item on page reload

--- ClientApp/components/navmenu/navmenu.ts
+++ ClientApp/components/navmenu/navmenu.ts
@@ -1,7 +1,7 @@
 ﻿import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import Axios from 'axios';
-
+import $ from 'jquery';
 
 @Component
 export default class NavMenuComponent extends Vue {
@@ -17,5 +17,38 @@ export default class NavMenuComponent extends Vue {
             }).catch(error => {
                 console.log(error);
             });
+
+        this.setActiveMenuPageLoad();
+        this.activeMenuItemEventListener();
+    };
+    setActiveMenuPageLoad() {
+        // get the current route
+        var path = window.location.pathname;
+        // get the nav-link with the href equal to the current route
+        var navs = $('.nav-link').filter((index, link) => {
+            var attrib = ($(link)[0].attributes.getNamedItem('href') as Attr);
+            if (attrib != null) {
+                console.log(attrib.value);
+                return attrib.value == path;
+            }
+            return false;
+        });
+        if (navs.length > 0) {
+            this.setActiveMenuItem(navs[0]);
+        }
+    };
+    activeMenuItemEventListener() {
+        var current = this;
+        $('.nav-link').on('click', function (event) {
+            current.setActiveMenuItem(event.target);
+        });
+    };
+
+    setActiveMenuItem(navItem: Element) {
+        // remove active css class from all nav-links
+        $('.nav-link').removeClass('active');
+
+        // then add .active css class to 'new_active_item' to show it as active.
+        $(navItem).addClass('active');
     }
 }
