4999808abbe50caf7297f11ef0e5c7a08acd0d60
tmacharia <timothy.macharia@outlook.com>
2019-09-02 05:55:58 +0300

    database migrations

---
+++ Models/ContextFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Shop.Web.Models
+{
+    /// <summary>
+    /// Represents a design time implementation used by EntityFramework to create database tables(schemas)
+    /// when you run migrations using the following commands:
+    /// 
+    /// dotnet ef migrations add Initial
+    /// dotnet ef database update
+    /// 
+    /// Readmore:
+    /// <see cref="https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/"/>
+    /// </summary>
+    public class ContextFactory : IDesignTimeDbContextFactory<ShopDbContext>
+    {
+        private readonly IConfiguration configuration;
+        public ContextFactory()
+        {
+            var builder = new ConfigurationBuilder()
+                          .SetBasePath(Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            configuration = builder.Build();
+        }
+        public ShopDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ShopDbContext> builder = new DbContextOptionsBuilder<ShopDbContext>();
+            builder.UseSqlServer(configuration["ConnectionString"]);
+            return new ShopDbContext(builder);
+        }
+    }
+}
--- Models/ShopDbContext.cs
+++ Models/ShopDbContext.cs
@@ -18,16 +18,14 @@ namespace Shop.Web.Models
         {
             base.OnModelCreating(builder);
 
-            Database.EnsureCreated();
-
             // seed database with initial shops
             List<ShopModel> shops = new List<ShopModel>()
             {
                 new ShopModel(){Id =1,Name="Amazon",Description="Electronics",Distance=25,ImageUrl=""},
-                new ShopModel(){Id =1,Name="Macy's",Description="Clothes & Apparel",Distance=48,ImageUrl=""}
+                new ShopModel(){Id =2,Name="Macy's",Description="Clothes & Apparel",Distance=48,ImageUrl=""}
             };
 
-            builder.Entity<ShopModel>().HasData(shops);
+            builder.Entity<ShopModel>().HasData(shops.ToArray());
         }
     }
 }
