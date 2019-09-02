6e87e69ac8b6c0ce6262e9e936c91c68dc53e4f0
tmacharia <timothy.macharia@outlook.com>
2019-09-03 02:05:15 +0300

    minor edits & code refactorings

--- ClientApp/boot.ts
+++ ClientApp/boot.ts
@@ -2,12 +2,13 @@ import './css/site.css';
 import 'bootstrap';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import $ from 'jquery';
 Vue.use(VueRouter);
 
 const routes = [
-    { path: '/', component: require('./components/home/home.vue.html') },
-    { path: '/shops', component: require('./components/home/home.vue.html') },
-    { path: '/shops/preferred', component: require('./components/preferred/preferred.vue.html') },
+    { path: '/', component: require('./components/shops/shops.vue.html') },
+    { path: '/shops', component: require('./components/shops/shops.vue.html') },
+    { path: '/shops/preferred', component: require('./components/preferred-shops/preferred-shops.vue.html') },
     { path: '/signin', component: require('./components/account/signin.vue.html') },
     { path: '/signup', component: require('./components/account/signup.vue.html') },
     { path: '/account', component: require('./components/account/account.vue.html') },
@@ -17,7 +18,9 @@ new Vue({
     el: '#app-root',
     router: new VueRouter({ mode: 'history', routes: routes }),
     render: h => h(require('./components/app/app.vue.html')),
-    mounted: function () {
+    created: function () {
         
+    },
+    mounted: function () {
     }
 });
--- ClientApp/components/navmenu/navmenu.ts
+++ ClientApp/components/navmenu/navmenu.ts
@@ -3,6 +3,44 @@ import { Component } from 'vue-property-decorator';
 import Axios from 'axios';
 import $ from 'jquery';
 
+function addMenuItemRouteChangedEventListener() {
+    $('.nav-link').on('click', function (event) {
+        setActiveMenuItem(event.target);
+    });
+}
+function setActiveMenuItemOnPageLoad() {
+    /*********************
+     * To give vue.js time to finish mounting all objects,
+     * we will have implement a delay, otherwise this will result
+     * in the wrong results. 100 milliseconds is enough to achieve
+     * the desired results.
+     * 
+     ***********/
+    setTimeout(() => {
+        // get the current route
+        var path = window.location.pathname;
+        // get the nav-link with the href equal to the current route
+        var navs = $('.nav-link').filter((index, nav_link) => {
+            var href_attribute = ($(nav_link)[0].attributes.getNamedItem('href') as Attr);
+            if (href_attribute != null) {
+                console.log(href_attribute.value);
+                return href_attribute.value == path;
+            }
+            return false;
+        });
+        if (navs.length > 0) {
+            setActiveMenuItem(navs[0]);
+        }
+    }, 100);
+}
+function setActiveMenuItem(navItem: Element) {
+    // remove active css class from all nav-links
+    $('.nav-link').removeClass('active');
+
+    // then add .active css class to 'new_active_item' to show it as active.
+    $(navItem).addClass('active');
+}
+
 @Component
 export default class NavMenuComponent extends Vue {
     loggedIn: boolean = false;
@@ -14,40 +52,11 @@ export default class NavMenuComponent extends Vue {
             .then(response => {
                 this.loggedIn = response.data.isAuthenticated;
                 this.user = response.data.name;
+
+                setActiveMenuItemOnPageLoad();
+                addMenuItemRouteChangedEventListener();
             }).catch(error => {
                 console.log(error);
             });
-
-        this.setActiveMenuPageLoad();
-        this.activeMenuItemEventListener();
     };
-    setActiveMenuPageLoad() {
-        // get the current route
-        var path = window.location.pathname;
-        // get the nav-link with the href equal to the current route
-        var navs = $('.nav-link').filter((index, nav_link) => {
-            var href_attribute = ($(nav_link)[0].attributes.getNamedItem('href') as Attr);
-            if (href_attribute != null) {
-                return href_attribute.value == path;
-            }
-            return false;
-        });
-        if (navs.length > 0) {
-            this.setActiveMenuItem(navs[0]);
-        }
-    };
-    activeMenuItemEventListener() {
-        var current = this;
-        $('.nav-link').on('click', function (event) {
-            current.setActiveMenuItem(event.target);
-        });
-    };
-
-    setActiveMenuItem(navItem: Element) {
-        // remove active css class from all nav-links
-        $('.nav-link').removeClass('active');
-
-        // then add .active css class to 'new_active_item' to show it as active.
-        $(navItem).addClass('active');
-    }
 }
