b656d554f8ea34b4ae08c29ebeffcf5b05a8beb3
tmacharia <timothy.macharia@outlook.com>
2019-09-02 10:40:21 +0300

    adds signin link to header navbar & edits app name

--- Globals.cs
+++ Globals.cs
@@ -4,7 +4,7 @@ namespace Shop.Web
 {
     public static class Globals
     {
-        public const string AppName = "Shop Viewer";
+        public const string AppName = "Shopper Store";
 
         public static Random random = new Random();
         public static int RandomNum(int max = 1000) => random.Next(0, max);
